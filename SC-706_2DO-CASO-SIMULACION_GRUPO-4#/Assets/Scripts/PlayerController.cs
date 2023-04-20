8e6f400a620b9c8cc49c36dd0ac1f4a4522e457a
adrianoscr <adrianos1802@gmail.com>
2023-04-20 15:19:22 -0600

    Movimiento jugador y camara completo

--- SC-706_2DO-CASO-SIMULACION_GRUPO-4#/Assets/Scripts/PlayerController.cs
+++ SC-706_2DO-CASO-SIMULACION_GRUPO-4#/Assets/Scripts/PlayerController.cs
@@ -5,36 +5,166 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float jumpSpeed = 5.0f;
-    public float gravity = 20.0f;
+    [Header("Move")]
+    [SerializeField]
+    float walkFactor = 5.0F;
 
-    private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
+    [SerializeField]
+    float runFactor = 5.0F;
 
-    void Start()
+    [SerializeField]
+    float rotationFactor = 10.0F;
+
+
+    [Header("Jump")]
+    [SerializeField]
+    float gravityMultiplier = 3.0F;
+
+    [SerializeField]
+    float jumpForce = 8.0F;
+
+    [SerializeField]
+    int maximunNumberOfJumps = 2;
+
+    CharacterController character;
+
+    Vector3 direccion;
+
+    float inputX;
+    float inputZ;
+    float magnitude;
+    float gravityY;
+    float velocityY;
+
+    bool isRunnig;
+    bool isMovePressed;
+    bool isJumpPressed;
+
+    int numberOfJumps;
+
+    void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        character = GetComponent<CharacterController>();
+
+        gravityY = Physics.gravity.y;
+
     }
 
     void Update()
     {
-        if (controller.isGrounded)
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+        handleInputs();
+        HandleGravity();
+        handleMove();
+        handleRotation();
+    }
 
-            if (Input.GetButton("Jump"))
+
+    void HandleGravity()
+    {
+        bool isGrounded = IsGrounded();
+
+        if (isGrounded)
+        {
+            if (velocityY < -1.0F)
             {
-                moveDirection.y = jumpSpeed;
+                velocityY = -1.0F;
+            }
+
+            if (isJumpPressed)
+            {
+                handleJump();
+                StartCoroutine(WaitForGroundCorutine());
             }
         }
+        else
+        {
 
-        moveDirection.y -= gravity * Time.deltaTime;
+            if (isJumpPressed && maximunNumberOfJumps > 1)
+            {
+                handleJump();
+            }
+            velocityY += gravityY * gravityMultiplier * Time.deltaTime;
+        }
 
-        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+    void handleInputs()
+    {
+
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputZ = Input.GetAxisRaw("Vertical");
+
+
+        //DETECTAR CUANDO SE ESTA MOVIENDO
+        isMovePressed = inputX != 0.0F || inputZ != 0.0F;
+        isRunnig = Input.GetButton("Fire3");
+        isJumpPressed = Input.GetButtonDown("Jump");
+    }
+
+    void handleJump()
+    {
+        if (numberOfJumps > maximunNumberOfJumps)
+        {
+            return;
+        }
+
+        numberOfJumps++;
+        velocityY = jumpForce / numberOfJumps;
+
+    }
+
+    void handleMove()
+    {
+        direccion = new Vector3(inputX, 0.0F, inputZ);
+        direccion = Camera.main.transform.forward * direccion.z + Camera.main.transform.right * direccion.x;
+        direccion.y = 0.0F;
+
+        //AVERIGUAR DIRECCION DEL MOVIMIENTO
+        magnitude = Mathf.Clamp01(direccion.magnitude);
+        direccion.Normalize();
+
+        //Correr o caminar
+        Vector3 velocity = direccion * magnitude *
+            (isRunnig
+                ? runFactor
+                : walkFactor);
+
+        //CON ESTO LE DAMOS INPULSO HACIA EL PISO
+        velocity.y = velocityY;
+
+        //MOVIMIENTO UNIFORME DURANTE EL TIEMPO
+        character.Move(velocity * Time.deltaTime);
+    }
+    void handleRotation()
+    {
+
+        if (isMovePressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direccion, Vector3.up);
+
+            // Si el personaje va hacia atrás, mantener la rotación actual
+            if (inputZ < 0)
+            {
+                targetRotation = transform.rotation;
+            }
+
+            transform.rotation =
+                Quaternion.Slerp(transform.rotation, targetRotation, rotationFactor * Time.deltaTime);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return character.isGrounded;
+    }
+
+    IEnumerator WaitForGroundCorutine()
+    {
+
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(() => IsGrounded());
+        numberOfJumps = 0;
+
     }
 }
