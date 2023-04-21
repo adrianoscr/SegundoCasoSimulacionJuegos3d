e861390293fb2f5c2a2235ee37417484b8acd3a5
adrianoscr <adrianos1802@gmail.com>
2023-04-20 18:43:59 -0600

    Este es el último Cambio
    Se agregó para solucionar un bug que no habías encontrado. Ya los policias te cazan

--- SC-706_2DO-CASO-SIMULACION_GRUPO-4#/Assets/Scripts/policia.cs
+++ SC-706_2DO-CASO-SIMULACION_GRUPO-4#/Assets/Scripts/policia.cs
@@ -1,44 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class policia : MonoBehaviour
 {
-    public float Rango;
+    enum PatrolStates { Patroling, Chasing, Attacking }
+    [Header("Target")]
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    LayerMask WhatIsPlayer;
 
-    public LayerMask personaje;
-    public Transform jugador;
-    public float velocidad;
-
-
-    bool Alerta;
-
-    
-    void Start()
+    [Header("Patrol")]
+    [SerializeField]
+    LayerMask WhatIsGround;
+    [SerializeField]
+    float walkPointRange;
+    [SerializeField]
+    float sightRange;
+    [Header("Attack")]
+    [SerializeField]
+    float attackRange;
+    [SerializeField]
+    float attackRate = 0.5F;
+    NavMeshAgent agent;
+    Vector3 walkPoint;
+    bool isWalkPointSet;
+    bool isAttacking;
+    bool isTargetOnSight;
+    bool isTargetInAttackRange;
+    PatrolStates currentState;
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    
     void Update()
     {
-        Alerta = Physics.CheckSphere(transform.position, Rango, personaje);
+        isTargetOnSight = Physics.CheckSphere(transform.position, sightRange, WhatIsPlayer);
+        isTargetInAttackRange = Physics.CheckSphere(transform.position, attackRate, WhatIsPlayer);
+        currentState = isTargetOnSight && isTargetInAttackRange ?
+            PatrolStates.Attacking : isTargetOnSight && !isTargetInAttackRange ?
+            PatrolStates.Chasing : PatrolStates.Patroling;
 
-        if(Alerta == true)
+        switch (currentState)
         {
-            transform.LookAt(new Vector3(jugador.position.x,transform.position.y,jugador.position.z));
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(jugador.position.x, transform.position.y, jugador.position.z), velocidad * Time.deltaTime);
+            case PatrolStates.Attacking:
+                HandleAttack();
+                break;
+            case PatrolStates.Chasing:
+                HandleChase();
+                break;
+            case PatrolStates.Patroling:
+                HandlePatrol();
+                break;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, walkPointRange);
+        Gizmos.color = Color.yellow; Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.color = Color.red; Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
+    private void HandleChase()
+    {
+        agent.SetDestination(target.position);
+
+    }
+    private void HandlePatrol()
+    {
+        if (!isWalkPointSet)
+        {
+            FindNextWalkPoint();
+            if (isWalkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+
+            }
+
         }
 
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if (distanceToWalkPoint.magnitude < 1)
+        {
+            isWalkPointSet = false;
+        }
     }
 
-    private void OnDrawGizmos()
+    private void FindNextWalkPoint()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, Rango);
+        float positionX = Random.Range(-walkPointRange, walkPointRange);
+        float positionZ = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + positionX, transform.position.y, transform.position.z + positionZ);
 
-
-
+        if (Physics.Raycast(walkPoint, -transform.up, WhatIsGround))
+        {
+            isWalkPointSet = true;
+        }
+    }
+    private void HandleAttack()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(target); if (!isAttacking)
+        {
+            isAttacking = true;
+            Invoke(nameof(ResetAttack), attackRate);
+        }
     }
 
+    private void ResetAttack()
+    {
+        isAttacking = false;
+    }
 }
